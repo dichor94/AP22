6d3923ef56c7e5009c528b5a3e30ff36a079e0f6
dichor94 <dichor@alumnatflorida.es>
2024-09-30 13:54:06 +0200

    Añado el documento 2.php con el html, inicio a partir de aquí el código php

---
+++ 2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Ejercicio 2</h1>
+    <br>
+    <strong>Ejercicio2: </strong>Se requiere conocer el área de un rectángulo. Como se sabe, para poder obtener el área del rectángulo, primeramente se tiene que conocer la base y la altura, y una vez obtenidas se presenta el resultado.
+    <br><br>
+
+    
+
+    
+</body>
+</html>
